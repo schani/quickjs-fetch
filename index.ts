2eea67cd61b41f1d3409a7d76c1b39f842f97108
Mark Probst <mark.probst@gmail.com>
2022-11-27 17:02:22 -0500

    Set limits

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { getQuickJS, QuickJSDeferredPromise, QuickJSWASMModule, Scope } from "quickjs-emscripten";
+import { getQuickJS, QuickJSWASMModule, Scope } from "quickjs-emscripten";
 import fetch from "node-fetch";
 
 const code = `
@@ -12,94 +12,137 @@ return films[Math.floor(Math.random() * films.length)];
 })()
 `;
 
-const preamble = `
-this.__responsePrototype = {
+// const code = `
+// (async function () {
+//   let x = 0;
+//   for (let i = 0; i < 1000000000; i++) {
+//     if (x > 0) {
+//       x -= i;
+//     } else {
+//       x += i;
+//     }
+//   }
+//   return x;
+// })()
+// `;
+
+const responsePrototypeCode = `
+({
   async json() {
     return JSON.parse(await this.text());
   }
-}
+})
 `;
 
 async function test(quickJS: QuickJSWASMModule): Promise<void> {
-  await Scope.withScopeAsync(async scope => {
-    const vm = scope.manage(quickJS.newContext());
-    scope.manage(vm.unwrapResult(vm.evalCode(preamble)));
+    await Scope.withScopeAsync(async (scope) => {
+        const runtime = scope.manage(quickJS.newRuntime());
+        runtime.setMemoryLimit(1024 * 1024 * 512);
+        runtime.setMaxStackSize(1024 * 1024 * 2);
 
-    const responseClass = scope.manage(vm.getProp(vm.global, "__responsePrototype"));
+        const start = Date.now();
+        // Don't run for longer than 10 seconds
+        runtime.setInterruptHandler(() => {
+            console.log("interrupt");
+            return Date.now() - start > 10 * 1000;
+        });
 
-    const fetchFN = scope.manage(vm.newFunction("fetch", (urlHandle) => {
-      const url = vm.getString(urlHandle);
-      // urlHandle.dispose();
-      console.log("fetch", url);
-      // return vm.newNumber(123);
-      const qjsPromise = scope.manage(vm.newPromise());
-      fetch(url).then(res => {
-        if (!vm.alive) return;
-        if (res === undefined) {
-          qjsPromise.resolve(vm.undefined);
-          return;
-        }
-        const obj = scope.manage(vm.newObject(responseClass));
-        vm.setProp(obj, "ok", res.ok ? vm.true : vm.false);
+        const vm = scope.manage(runtime.newContext());
+        const responsePrototype = scope.manage(
+            vm.unwrapResult(vm.evalCode(responsePrototypeCode))
+        );
 
-        const textFN = scope.manage(vm.newFunction("text", () => {
-          const qjsTextPromise = scope.manage(vm.newPromise());
-          res.text().then(str => {
-            if (!vm.alive) return;
-            qjsTextPromise.resolve(scope.manage(vm.newString(str)));
-          }).catch(_e => {
-            if (!vm.alive) return;
-            qjsTextPromise.reject(scope.manage(vm.newError("Error")));
-          });
-          qjsTextPromise.settled.then(vm.runtime.executePendingJobs);
-          return qjsTextPromise.handle;
-        }));
-        vm.setProp(obj, "text", textFN);
-        qjsPromise.resolve(obj);
-      }).catch(_e => {
-        if (!vm.alive) return;
-        qjsPromise.reject(scope.manage(vm.newError("Error")));
-      });
-      qjsPromise.settled.then(vm.runtime.executePendingJobs);
-      return qjsPromise.handle;
-    }));
-    vm.setProp(vm.global, "fetch", fetchFN);
+        const fetchFN = scope.manage(
+            vm.newFunction("fetch", (urlHandle) => {
+                const url = vm.getString(urlHandle);
+                // urlHandle.dispose();
+                console.log("fetch", url);
+                // return vm.newNumber(123);
+                const qjsPromise = scope.manage(vm.newPromise());
+                fetch(url)
+                    .then((res) => {
+                        if (!vm.alive) return;
+                        if (res === undefined) {
+                            qjsPromise.resolve(vm.undefined);
+                            return;
+                        }
+                        const obj = scope.manage(
+                            vm.newObject(responsePrototype)
+                        );
+                        vm.setProp(obj, "ok", res.ok ? vm.true : vm.false);
 
-    // const result = vm.evalCode(`Object.getOwnPropertyNames(this).join(",")`)
-    const result = vm.evalCode(code);
+                        const textFN = scope.manage(
+                            vm.newFunction("text", () => {
+                                const qjsTextPromise = scope.manage(
+                                    vm.newPromise()
+                                );
+                                res.text()
+                                    .then((str) => {
+                                        if (!vm.alive) return;
+                                        qjsTextPromise.resolve(
+                                            scope.manage(vm.newString(str))
+                                        );
+                                    })
+                                    .catch((_e) => {
+                                        if (!vm.alive) return;
+                                        qjsTextPromise.reject(
+                                            scope.manage(vm.newError("Error"))
+                                        );
+                                    });
+                                qjsTextPromise.settled.then(
+                                    vm.runtime.executePendingJobs
+                                );
+                                return qjsTextPromise.handle;
+                            })
+                        );
+                        vm.setProp(obj, "text", textFN);
+                        qjsPromise.resolve(obj);
+                    })
+                    .catch((_e) => {
+                        if (!vm.alive) return;
+                        qjsPromise.reject(scope.manage(vm.newError("Error")));
+                    });
+                qjsPromise.settled.then(vm.runtime.executePendingJobs);
+                return qjsPromise.handle;
+            })
+        );
+        vm.setProp(vm.global, "fetch", fetchFN);
 
-    const resultHandle = scope.manage(vm.unwrapResult(result));
+        // const result = vm.evalCode(`Object.getOwnPropertyNames(this).join(",")`)
+        const result = vm.evalCode(code);
 
-    console.log("result", vm.dump(resultHandle), vm.typeof(resultHandle));
+        const resultHandle = scope.manage(vm.unwrapResult(result));
 
-    const resolvedResult = await vm.resolvePromise(resultHandle);
+        console.log("result", vm.dump(resultHandle), vm.typeof(resultHandle));
 
-    const resolvedHandle = scope.manage(vm.unwrapResult(resolvedResult));
+        const resolvedResult = await vm.resolvePromise(resultHandle);
 
-    console.log("resolved result", vm.dump(resolvedHandle));
-  })
+        const resolvedHandle = scope.manage(vm.unwrapResult(resolvedResult));
 
-  // if (result.error) {
-  //   console.log("Execution failed:", vm.dump(result.error))
-  //   result.error.dispose();
-  // } else {
-  //   console.log("Success:", vm.dump(result.value))
-  //   result.value.dispose();
-  // }
+        console.log("resolved result", vm.dump(resolvedHandle));
+    });
+
+    // if (result.error) {
+    //   console.log("Execution failed:", vm.dump(result.error))
+    //   result.error.dispose();
+    // } else {
+    //   console.log("Success:", vm.dump(result.value))
+    //   result.value.dispose();
+    // }
 }
 
 async function main() {
-  const QuickJS = await getQuickJS();
+    const QuickJS = await getQuickJS();
 
-  for (let i = 0; i < 1; i++) {
-    try {
-      await test(QuickJS);
-    } catch (e: unknown) {
-      console.error(e);
+    for (let i = 0; i < 1; i++) {
+        try {
+            await test(QuickJS);
+        } catch (e: unknown) {
+            console.error(e);
+        }
     }
-  }
 
-  console.log("done");
+    console.log("done");
 }
 
-main()
+main();
